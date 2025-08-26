5bd72fd1db11229b6d8c7058864858939c8be2d2
Kyrylo <idkHaveEmailForThisProject>
2025-08-26 20:53:31 +0200

    fully working AddEventcontroller but need few improvements in future

--- AddEventController.java
+++ AddEventController.java
@@ -14,8 +14,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class AddEventController implements Initializable {
+  //all good here except CheckStatments method but in futer fix i can make him better
     @FXML
     private ChoiceBox MothChoiceBox = new ChoiceBox<String>();
     @FXML
@@ -24,22 +26,38 @@ public class AddEventController implements Initializable {
     private ChoiceBox YearChoiceBox = new ChoiceBox<Integer>();
     @FXML
     private Button AcceptButton;
+    @FXML
+    private Button AcceptButton1;
+    @FXML
+    private Button AcceptButton11;
+    @FXML
+    private ChoiceBox TypeChoiceBox = new ChoiceBox<String>();
+    @FXML
+    private TextField NameOfEventTextField;
+
+
+      String[] ListOfEvents = { "Conference","Workshop","Webinar","Seminar","Concert","Festival","Meetup","Networking Event",
+      "Fundraiser","Charity Event","Exhibition","Trade Show","Launch Event","Panel Discussion","Hackathon","Award Ceremony",
+      "Sports Event","Comedy Show","Theater Performance","Religious Event","Political Rally","Training Session","Retreat","Online Course",
+      "Job Fair","Book Signing","Art Opening","Community Gathering","Movie Screening","Gaming Tournament"};
 
       String[] ListOfMoth = { "January","February","March","April","May","June",
      "July","August","September","October","November","December"};
+
       Integer [] Days = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+
       Integer [] Years = {2026,2027,2028,2029,2030};
+
       ObservableList<String> MothList = FXCollections.observableArrayList(ListOfMoth);  
       ObservableList<Integer> DaysList = FXCollections.observableArrayList(Days);
-      ObservableList<Integer> YearList = FXCollections.observableArrayList();
+      ObservableList<Integer> YearList = FXCollections.observableArrayList(Years);
+      ObservableList<String> EventsList = FXCollections.observableArrayList(ListOfEvents);
 
 
         @Override
         public void initialize(URL arg0, ResourceBundle arg1) {
-          // for(int year:Years){
-          //   YearList.add(Integer.valueOf(year));
-          // }
-          YearList.addAll(Years);
+
+          TypeChoiceBox.getItems().addAll(EventsList);
           MothChoiceBox.getItems().addAll(MothList);
           DayChoiceBox.getItems().addAll(DaysList);
           YearChoiceBox.getItems().addAll(YearList);
@@ -71,19 +89,41 @@ public class AddEventController implements Initializable {
             }
            });
         }
+        //this method not working properlly like i want hin to work but i hope in future i can fix him to 
+        //work how i planned
         private String CheckchoiceBoxes(){
           String StatusOfChoiceBox;
-          if(MothChoiceBox.getItems().isEmpty()&&!DayChoiceBox.getItems().isEmpty()&&!YearChoiceBox.getItems().isEmpty()){
+          if(MothChoiceBox.getValue()==null && DayChoiceBox.getValue() != null && YearChoiceBox.getValue() != null ){
            return StatusOfChoiceBox = "MOTH_CHOICE_BOX_IS_EMPTHY";
-          }if(DayChoiceBox.getItems().isEmpty()&&!MothChoiceBox.getItems().isEmpty()&&!YearChoiceBox.getItems().isEmpty()){
+          }else if(DayChoiceBox.getValue()==null && MothChoiceBox.getValue() != null && YearChoiceBox.getValue() != null){
            return StatusOfChoiceBox = "DAY_CHOICE_BOX_IS_EMPTHY";
-          }if(YearChoiceBox.getItems().isEmpty()&&!MothChoiceBox.getItems().isEmpty()&&!DayChoiceBox.getItems().isEmpty()){
+          }else if(YearChoiceBox.getValue()==null && MothChoiceBox.getValue() != null && DayChoiceBox.getValue() != null){
            return StatusOfChoiceBox = "YEAR_CHOICE_BOX_IS_EMPTHY";
-          }if(MothChoiceBox.getItems().isEmpty()&&DayChoiceBox.getItems().isEmpty()&&YearChoiceBox.getItems().isEmpty()){
+          }else if(MothChoiceBox.getValue()==null && DayChoiceBox.getValue()==null && YearChoiceBox.getValue()==null){
            return StatusOfChoiceBox = "ALL_cHOICE_BOX_ARE_EMPTHY";
           }else{
             return StatusOfChoiceBox = "ALL_GOOD";
           }
+        }
+        @FXML
+        private void CheckNameOfEvvent(){
+          AcceptButton1.setOnAction(e ->{
+            if(NameOfEventTextField.getText().isEmpty()){
+              System.out.println("Please Enter Name Of Event");
+            }else{
+              System.out.println("All good with name ");
+            }
+          });
+        }
+        @FXML
+        private void CheckTypOfEvent(){
+          AcceptButton11.setOnAction(e ->{
+          if(TypeChoiceBox.getValue()==null){
+            System.out.println("choose type of event please");
+          }else{
+            System.out.println("all good with typr of event ");
+          }       
+          });
         }
     
       
