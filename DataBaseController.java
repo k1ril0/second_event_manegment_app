36ac510f5d065fd4ca6c5129ba031bfe0c785461
Kyrylo <idkHaveEmailForThisProject>
2025-09-05 23:52:39 +0200

    fast commit for data base where i created not fully working add event scheduel method but there wotk to do

--- DataBaseController.java
+++ DataBaseController.java
@@ -29,7 +29,7 @@ public class DataBaseController {
             AddStatment.setString(1,Username);
             AddStatment.setString(2, Password);
             int rowsAffected = AddStatment.executeUpdate();
-            while(rowsAffected >= 0){ 
+            while(rowsAffected >= 1){ 
                 System.out.println("Rows has been updated : " + rowsAffected);
                 AddConnection.close();
             } 
@@ -39,6 +39,29 @@ public class DataBaseController {
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+    //working method of adding events to the database but there is wor to do in AddEventController where i should 
+    // manage howiject list of objects inn paramtres of this void so this i hope i fix in next update 
+    // what will be time when i comeback from vacation 
+    public void AddEventToTheSceduel(String NameOfEvent,String TypeOfEvent,String DateOfEvent){
+       Connection AddEventToTheSceduelConnection = getConnection();
+       try{
+        String Sql_Command_For_Add_Event_To_Scheduel = "insert into eventssceduel(DateOfEvent,NameOfEvent,TypeOfEvent) values(?,?,?)";
+        if(AddEventToTheSceduelConnection !=null){
+          PreparedStatement ADD_EVENT_SCHEDUEL = AddEventToTheSceduelConnection.prepareStatement(Sql_Command_For_Add_Event_To_Scheduel);
+          ADD_EVENT_SCHEDUEL.setString(1, DateOfEvent);
+          ADD_EVENT_SCHEDUEL.setString(2, NameOfEvent);
+          ADD_EVENT_SCHEDUEL.setString(3, TypeOfEvent);
+          int rowsAffected = ADD_EVENT_SCHEDUEL.executeUpdate();
+          while(rowsAffected >= 1 ){
+          System.out.println("Rows has been updated : " + rowsAffected);
+          }
+        }else{
+            System.out.println("Problem has been ocured in AddEventToScheduel mathod from DataBaseController");
+        }
+       }catch(Exception e){
+        e.printStackTrace();
+       }
     }
     
 }
---
+++ SeeEvent.java
@@ -0,0 +1,14 @@
+package com.event.app;
+
+public class SeeEvent extends CalendarMenuController {
+
+
+   //i want form this method is geting data from #CalendarMenuController day,month,year and then sowing user event 
+   // that plllaned on this date
+
+    private void SeeEvent(int Day,String Month,int Year){
+    //    System.out.println(GetAllChoiceBox(ChoseYearCalendar, ChoseMothCalendar, ChoseDataCalendar));
+    }
+    
+    
+}
